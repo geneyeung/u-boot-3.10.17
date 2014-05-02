6c4158ad1ecdea68cebf02220446c88d03e9e092
geneyeung <geneyym@gmail.com>
2014-05-02 05:12:05 -0400

    initial version

---
+++ build_u-boot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+make ARCH=arm CROSS_COMPILE=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi- mrproper
+make ARCH=arm CROSS_COMPILE=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi- mx6qsabresd_config
+make ARCH=arm CROSS_COMPILE=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi-
+
---
+++ rebuild_u-boot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+make ARCH=arm CROSS_COMPILE=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi-
+
